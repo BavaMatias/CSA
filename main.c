ea54a7f192767d44bb64b830a943c9d615e096f2
Matias Bava <matias@parrot.hive.palinuro.cloud>
2025-09-20 21:29:31 -0300

    New Project

---
+++ main.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+
+int main() {
+	// Disable output buffering
+	setbuf(stdout, NULL);
+	setbuf(stderr, NULL);
+
+	// You can use print statements as follows for debugging, they'll be visible when running tests.
+	printf("Logs from your program will appear here!\n");
+
+    //variables
+	int server_fd, client_addr_len;
+	struct sockaddr_in client_addr;
+
+    //Crea un socket TCP para comunicación en red
+    //funcion socket---> int socket(int domain, int type, int protocol);
+    //domain---> AF_INET = IPv4, AF_INET6 = IPv6, AF_UNIX = sockets locales
+    //type---> SOCK_STREAM = TCP, SOCK_DGRAM = UDP
+	server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (server_fd == -1) {
+	printf("Socket creation failed: %s...\n", strerror(errno));
+	return 1;
+	}
+
+	// Since the tester restarts your program quite often, setting SO_REUSEADDR
+	// ensures that we don't run into 'Address already in use' errors
+	int reuse = 1;
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+	printf("SO_REUSEADDR failed: %s \n", strerror(errno));
+	return 1;
+	}
+
+	struct sockaddr_in serv_addr = { .sin_family = AF_INET ,
+									.sin_port = htons(6379),
+									.sin_addr = { htonl(INADDR_ANY) },
+								};
+
+	if (bind(server_fd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) != 0) {
+	printf("Bind failed: %s \n", strerror(errno));
+	return 1;
+	}
+
+	int connection_backlog = 5;
+	if (listen(server_fd, connection_backlog) != 0) {
+	printf("Listen failed: %s \n", strerror(errno));
+	return 1;
+	}
+
+	printf("Waiting for a client to connect...\n");
+	client_addr_len = sizeof(client_addr);
+
+	int client_fd=accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+	printf("Client connected\n");
+
+	// Leer una sola vez
+	char buffer[1024];
+	int n;	
+
+    while (1) {
+		n = read(client_fd, buffer, sizeof(buffer) - 1);
+		if (n <= 0) {
+			// Cliente cerró la conexión o error
+			break;
+		}
+
+		buffer[n] = '\0';  // Null terminate
+		printf("Received: %s\n", buffer);
+
+		// Responder PING
+		if (strstr(buffer, "PING") != NULL) {
+			const char *pong = "+PONG\r\n";
+			write(client_fd, pong, strlen(pong));
+		}
+	}
+
+
+	 close(server_fd);
+
+	return 0;
+}
